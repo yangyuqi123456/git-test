c4d5d04ba919fc97d01a853aa73769e0e8b4f148
yyq <2405334997@qq.com>
2022-07-16 16:52:59 +0800

    first commit

---
+++ src/main/java/gitTest.java
@@ -0,0 +1,11 @@
+/**
+ * gitTest
+ *
+ * @author shuai
+ * @date 2022/7/16
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
